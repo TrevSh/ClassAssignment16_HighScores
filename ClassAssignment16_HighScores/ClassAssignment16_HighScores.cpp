f805303cbd43786f20dd0c3a088a429254244689
TrevSh <trevonshelby@outlook.com>
2023-07-24 02:58:16 -0500

    Added notes about iterator dot operators.

--- ClassAssignment16_HighScores/ClassAssignment16_HighScores.cpp
+++ ClassAssignment16_HighScores/ClassAssignment16_HighScores.cpp
@@ -23,7 +23,7 @@ int main()
     int score;
     std::cout << "\nEnter a score to find: ";
     std::cin >> score;
-    iter = find(scores.begin(), scores.end(), score);
+    iter = find(scores.begin(), scores.end(), score);                       //find() searches a vector through a specified range until it finds and returns an iterator referencing the first matching element(third argument). Will return an iterator at end of range if none is found.
     if (iter != scores.end()) {
         std::cout << "Score found.\n";
     }
@@ -32,15 +32,15 @@ int main()
     }
 
     std::cout << "\nRandomizing scores.";
-    srand(static_cast<unsigned int>(time(0)));
-    std::random_shuffle(scores.begin(), scores.end());
+    srand(static_cast<unsigned int>(time(0)));                              //Seeding randomizer with time
+    std::random_shuffle(scores.begin(), scores.end());                      //random_shuffle randomizes elements of a sequence with iterators (being score.begin/end here). Wont change anything, will just shuffle.
     std::cout << "\nHigh Scores: \n";
     for (iter = scores.begin(); iter != scores.end(); ++iter) {
         std::cout << *iter << "\n";
     }
 
     std::cout << "\nSorting Scores\n";
-    std::sort(scores.begin(), scores.end());
+    std::sort(scores.begin(), scores.end());                                //Sort() sorts elements in a range in ascending order. Must use iterators aswell. Can be used on strings and other non-STL containers.
     std::cout << "\nHigh Scores: \n";
     for (iter = scores.begin(); iter != scores.end(); ++iter) {
         std::cout << *iter << "\n";
